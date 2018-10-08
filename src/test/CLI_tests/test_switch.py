6503f0d1a3a3a7e47e77995e3371d7a5a203cb3e
fabian <fabian@Acer-FM.debian.de>
2018-10-08 21:04:58 +0200

    Test argument-amount-check and gap-handling for CLI.switch

--- src/test/CLI_tests/test_switch.py
+++ src/test/CLI_tests/test_switch.py
@@ -5,9 +5,10 @@ Created on 05.09.2018
 '''
 import unittest
 import unittest.mock as mock
-from CLI import switch, RangeExpansionError, CLIRuntimeError
+from CLI import switch, RangeExpansionError, CLIRuntimeError, ArgumentAmountError,\
+    InputProcessingError
 from FileSet import FileSet
-from test.testing_tools import mock_assert_msg
+from test.testing_tools import mock_assert_msg, KeywordArgTuple
 
 
 mock_expand_range = mock.MagicMock(name='_expand_range')
@@ -68,7 +69,55 @@ class SwitchTests(unittest.TestCase):
             switch(None, test_args)
         
         mock_assert_msg(mock_switch_file_ranges.assert_not_called, [], "The CLI attempts to perform a switch operation even though no file set is selected.")
+    
+    def test_too_few_arguments(self):
+        """The CLI should recognize and raise an error when too few arguments are given."""
+        test_args = ['2-3', '~']
+        mock_expand_range.side_effect = lambda x: (2, 3)
+        
+        with self.assertRaises(ArgumentAmountError, msg="The FileSet fails to recognize when too few arguments are given."):
+            switch(self.test_set, test_args)
+        
+        mock_assert_msg(mock_switch_file_ranges.assert_not_called, [], "The CLI attempts to perform a switch operation even though there was an invalid number of arguments.")
+        
+    def test_too_many_arguments(self):
+        """The CLI should recognize and raise an error when too many arguments are given."""
+        test_args = ['2-3', '~', '4-5', 'x', 'y']
+        mock_expand_range.side_effect = lambda x: (2, 3)
+        
+        with self.assertRaises(ArgumentAmountError, msg="The FileSet fails to recognize when too many arguments are given."):
+            switch(self.test_set, test_args)
+        
+        mock_assert_msg(mock_switch_file_ranges.assert_not_called, [], "The CLI attempts to perform a switch operation even though there was an invalid number of arguments.")
+        
+    def test_kwarg_strip_gaps(self):
+        """The CLI should enable the user to choose strip_gaps as a gap handling method."""
+        test_args = ['2-3', '~', '4-5', '-sg']
+        mock_expand_range.side_effect = lambda x: (2, 3) if x.startswith('2') else (4, 5)
+        
+        switch(self.test_set, test_args)
+        
+        mock_assert_msg(mock_switch_file_ranges.assert_called_once_with, [(2, 3), (4, 5), KeywordArgTuple('strip_gaps', True)], "The CLI fails to provide access to the gap-handling option strip_gaps.")
+        
+    def test_kwarg_preserve_gaps(self):
+        """The CLI should enable the user to choose preserve_gaps as a gap handling method."""
+        test_args = ['2-3', '~', '4-5', '-pg']
+        mock_expand_range.side_effect = lambda x: (2, 3) if x.startswith('2') else (4, 5)
+        
+        switch(self.test_set, test_args)
+        
+        mock_assert_msg(mock_switch_file_ranges.assert_called_once_with, [(2, 3), (4, 5), KeywordArgTuple('preserve_gaps', True)], "The CLI fails to provide access to the gap-handling option strip_gaps.")
 
+    def test_invalid_kwarg_gap_handler(self):
+        """The CLI should recognize and raise an error if an invalid gap-handling option was chosen."""
+        test_args = ['2-3', '~', '4-5', '-xx']
+        mock_expand_range.side_effect = lambda x: (2, 3) if x.startswith('2') else (4, 5)
+        
+        with self.assertRaises(InputProcessingError, msg="The CLI fails to recognize and raise an error when an invalid gap-handling option was chosen"):
+            switch(self.test_set, test_args)
+        
+        mock_assert_msg(mock_switch_file_ranges.assert_not_called, [], "The CLI tries to eprform an operation even though an error was raised.")
+    
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
